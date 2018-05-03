ad638e64996c13b0631036a91918289ec4d9b8c7
Robert Jamróz <biuro@servicesit.pl>
2018-05-03 22:02:33 +0200

    simple way by pipe

--- index.js
+++ index.js
@@ -16,51 +16,16 @@ manager.addServer("192.168.33.10", 6380);
 // info
 console.log('Redis cluster 1.0.0', );
 
-var map = {};
-var mapDb = {};
 var counter = 0;
-
-
 
 // listen for clients
 var server = net.createServer({
 	allowHalfOpen :true
 }, function (socket) {
 
-	var selectDb = 0;
-	socket.on('data', function (data) {
-		console.log('command: ' + Utils.escape(data));
-
-		var splited = (data+'').split(" ");
-		var cmd = splited[0];
-		var key = splited[1];
-
-		var hash = key;
-
-		//if (!(hash in map)) {
-
-			map[hash] = new Number(counter % manager.clients.length);
-			mapDb[hash] = selectDb;
-			counter += 1;
-		//}
-
-		manager.write(map[hash], data, function (data) {
-
-			socket.write( data );
-			console.log('proxy: ' + Utils.escape(data+''));
-			
-		});
-
-	});
-
-	socket.on('end', function(){
-		console.log('client end');
-		socket.destroy();
-	});
-
-	//socket.pipe(socket);
-
-
+	
+	socket.pipe(manager.clients[counter%manager.clients.length]).pipe(socket);
+	counter+=1;
 });
 
 server.listen(1313, '127.0.0.1');
